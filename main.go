a8bed2ef475668b23e222dbd414e542b8b5a4218
Nadyan <nadyan_sp@hotmail.com>
2022-11-15 16:41:01 -0300

    save and read logs

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -33,7 +35,7 @@ func executeOption(option int) {
 	case 1:
 		initMonitoring()
 	case 2:
-		fmt.Println("Executing 2")
+		printLogs()
 	case 3:
 		fmt.Println("\nExiting app...")
 		os.Exit(0)
@@ -43,19 +45,20 @@ func executeOption(option int) {
 }
 
 func initMonitoring() {
+	fmt.Print("\033[H\033[2J")
 	fmt.Println("\nMonitoring websites...")
 
 	websites := readWebsitesFile()
 	for _, site := range websites {
 		res, err := http.Get(site)
-		printResult(site, res, err)
+		printAndSaveStatus(site, res, err)
 	}
 }
 
 func readWebsitesFile() []string {
 	var websites []string
 
-	file, err := os.Open("websites.txt")
+	file, err := os.Open("files/websites.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return websites
@@ -78,17 +81,41 @@ func readWebsitesFile() []string {
 	return websites
 }
 
-func printResult(site string, res *http.Response, err error) {
-	greenColor := "\033[32m"
-	redColor := "\033[31m"
-	resetColor := "\033[0m"
-
-	status := greenColor + "Online" + resetColor
+func printAndSaveStatus(site string, res *http.Response, err error) {
+	status := "Online"
 
 	if res == nil || res.StatusCode != 200 {
-		status = redColor + "Offline" + resetColor
+		status = "Offline"
 	}
 
 	fmt.Println(site, "->", status)
+	saveLog(site, status)
+}
 
+func saveLog(site string, status string) {
+
+	timestamp := time.Now().Local().String()
+
+	file, err := os.OpenFile("files/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Error saving in log: ", err)
+		return
+	}
+
+	file.WriteString(timestamp + " " + site + " -> " + status + "\n")
+
+	file.Close()
+}
+
+func printLogs() {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("\nPrevious status:")
+
+	file, err := ioutil.ReadFile("files/log.txt")
+	if err != nil {
+		fmt.Println("Error reading logs:", err)
+		return
+	}
+
+	fmt.Println(string(file))
 }
